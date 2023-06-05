a0ed5137f320fcc768d690f1570accd7d01dde86
dong-geon-Lee <ch9716@naver.com>
2023-06-05 23:43:35 +0900

    더미데이터 추가 및 렌더링

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,3 +1,4 @@
+import dataItems from "@/data/data";
 import React from "react";
 
 const CollectionItem = ({ children }: any) => {
@@ -11,9 +12,7 @@ const CollectionItem = ({ children }: any) => {
 const Main = () => {
   return (
     <div className="w-[80%] mx-auto">
-      <h1 className="text-[4.8rem] text-center mt-[10%]">
-        Explore Collections
-      </h1>
+      <h1 className="text-[4.8rem] text-center mt-[10%]">물품 둘러보기</h1>
       <ul className="flex items-center w-full justify-center gap-[2rem] text-[2rem] text-center mt-[3.2rem]">
         <CollectionItem>책</CollectionItem>
         <CollectionItem>영화</CollectionItem>
@@ -22,6 +21,34 @@ const Main = () => {
         <CollectionItem>카드</CollectionItem>
         <CollectionItem>기타</CollectionItem>
       </ul>
+
+      <div className="cards my-[3.6rem] flex flex-wrap justify-around gap-[3.2rem]">
+        {dataItems.map((data) => (
+          <div className="card w-[30rem] h-[40rem]">
+            <img
+              src={data.image}
+              alt={data.image}
+              className="flex-1 w-[30rem] h-[20rem]"
+            />
+            <div className="flex-1 p-[3.6rem] py-[3rem]">
+              <h1 className="text-[2rem] font-bold leading-[3rem]">
+                {data.title}
+              </h1>
+              <label className="text-[2rem] font-normal leading-[3rem]">
+                {data.price}
+              </label>
+              <p className="text-[1.6rem] font-normal leading-[3rem]">
+                {data.location}
+              </p>
+              <div className="flex gap-[1.2rem] text-[1.6rem] font-normal leading-[3rem]">
+                <span>관심 {data.interest}</span>
+                <span>댓글 {data.comments}</span>
+                <span className="flex">{data.status}</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
